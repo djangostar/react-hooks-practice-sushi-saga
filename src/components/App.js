d4e752df4b8d461b431ba6340889734dbf0d5cdf
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-03-24 12:17:26 -0700

    commit tree b30b078fdee384998ea48828431c2c38c8865905

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([])
+  const [fourSushis, setFourSushis] = useState([])
+  const [budget, setBudget] = useState(100);
+
+  useEffect(() => {
+    fetch(API)
+    .then(res => res.json())
+    .then(data => {
+      const allSushis = data.map(sushi =>{
+        return {...sushi, eaten: false }
+      })
+      const four = allSushis.splice(0, 4)
+      setSushis(allSushis)
+      setFourSushis(four)
+    })
+  }, [])
+
+  const getSushi = () => {
+    const allSushis = sushis
+    const four = sushis.splice(0, 4)
+    setSushis(allSushis)
+    setFourSushis(four)
+
+  }
+
+  const eatSushi = (e) => {
+    if (budget - parseInt(e.target.dataset.price) >= 0) {
+      const newSushis = fourSushis.map(sushi => sushi.id == e.target.id ? {...sushi, eaten: true} : sushi)
+      setFourSushis(newSushis)
+      const newBudget = budget - parseInt(e.target.dataset.price)
+      setBudget(newBudget)
+    }
+  }
+  console.log()
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer fourSushi={fourSushis} eatSushi={eatSushi} getSushi={getSushi} />
+      <Table fourSushis={fourSushis} budget={budget} />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,19 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+function SushiContainer({ fourSushi, eatSushi, getSushi }) {
+  const sushisList = fourSushi.filter(sushi => !sushi.eaten).map(s => 
+    <Sushi 
+        sushi={s} eatSushi={eatSushi}
+    />
+    )
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushisList}
+      <MoreButton getSushi={getSushi}/>
+      {/* <Sushi /> */}
     </div>
   );
 }
